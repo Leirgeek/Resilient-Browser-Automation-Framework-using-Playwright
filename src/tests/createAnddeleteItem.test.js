c96ab95f773446baaad37098214b48fb50897fdf
leirgeek <gabrielchege@gmail.com>
2025-10-31 16:42:02 +0300

    Added test cases to assert adding array items using a for loop.

--- src/tests/createAnddeleteItem.test.js
+++ src/tests/createAnddeleteItem.test.js
@@ -4,15 +4,17 @@ import {test, expect} from '@playwright/test';
 test('create a user', async ({page})=>{
     //access the page
     await page.goto('https://demo.playwright.dev/todomvc');
+    const counter = page.locator('.todo-count');
     await page.fill('.new-todo','james');
     await page.keyboard.press('Enter');
     await expect(page.locator('.todo-list li').nth(0)).toHaveText('james');
+    //await expect(counter).toContainText('1 item left');
     await page.fill('.new-todo', 'lewis');
     await page.keyboard.press('Enter');
     await expect(page.locator('.todo-list li').nth(1)).toHaveText('lewis');
-    //await page.getByPlaceholder('What needs to be done?').fill('James');
-   // await expect(page.locator('.todo-list li')).toHaveText('james');
-   // await page.locator('#todo-count').expect(page.getByText('4'));
+    
+    //assert the count increament 
+   await expect(page.locator('.todo-count')).toHaveText('2 items left');
 
 
 })
